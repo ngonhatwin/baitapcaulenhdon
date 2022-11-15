2e02dd6d1d30006dfb202bd3c7f97ba149158d3a
NgoNhatWin <117475856+ngonhatwin@users.noreply.github.com>
2022-11-15 09:38:40 +0700

    Add files via upload

---
+++ vd03.cpp
@@ -0,0 +1,10 @@
+#include "stdio.h"
+ int main(){
+    int a, b, tong;
+    printf ("\n hay nhap hai so a, b = ");
+    scanf("%d%d", &a, &b);
+    
+    tong= a + b;
+    
+    printf ("tong= %d", tong );
+}
